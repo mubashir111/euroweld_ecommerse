dde90ca506e67d6e715fc75c551e8891b37a8e61
mubashir111 <muba4shir@gmail.com>
2026-02-17 00:29:19 +0530

    changes 3

--- js/product-detail.js
+++ js/product-detail.js
@@ -3,18 +3,14 @@ $(document).ready(function () {
     var urlParams = new URLSearchParams(window.location.search);
     var productId = urlParams.get('id');
 
-    // 2. Default to 'wood-chair' (or any first item) if no ID is present, OR handle error
-    // For this template, we can default to 'chain-block' if nothing is specified for demo purposes
+    // 2. Redirect to products page if no ID is present
     if (!productId) {
-        // Option A: Default to first item
-        // productId = 'chain-block';
-
-        // Option B: Do nothing (let static HTML show) - but we want to be dynamic.
-        // Let's check if the static HTML has a "placeholder" that we should overwrite.
+        window.location.href = 'products.html';
+        return;
     }
 
     // 3. Look up product data
-    var product = productsData[productId || 'chain-block']; // Fallback to chain-block for now if null
+    var product = productsData[productId];
 
     if (product) {
         // 4. Update the Page Content
@@ -24,22 +20,24 @@ $(document).ready(function () {
         $('.shop-detail h4').text(product.name);
 
         // Images (Main Slider)
+        // FlexSlider specific:
+        var $slider = $('.images-slider');
         var sliderHtml = '<ul class="slides">';
         product.images.forEach(function (img) {
             sliderHtml += '<li data-thumb="' + img + '"> <img class="img-responsive" src="' + img + '" alt=""> </li>';
         });
         sliderHtml += '</ul>';
 
-        // We need to destroy/re-init flexslider or just replace content if it hasn't loaded yet.
-        // Since this runs on ready, flexslider might have already init. 
-        // A safer way for flexslider dynamic updates often involves more complex API use.
-        // For simplicity, we might simple replace the HTML and hope for main.js to pick it up?
-        // Actually, main.js inits flexslider on window.load. If we run this on document.ready, we are ahead of window.load!
-        $('.images-slider').html(sliderHtml);
+        // Destroy existing flexslider instance if initialized to allow clean re-init
+        if ($slider.data('flexslider')) {
+            $slider.flexslider('destroy');
+        }
+
+        $slider.html(sliderHtml);
 
         // Re-initialize FlexSlider
-        $('.images-slider').removeData("flexslider");
-        $('.images-slider').flexslider({
+        // We use a slight timeout to ensure DOM is ready if needed, but synchronous usually works here
+        $slider.flexslider({
             animation: "fade",
             controlNav: "thumbnails"
         });
@@ -62,9 +60,12 @@ $(document).ready(function () {
 
         // "Add to Quote" Button Data
         var $btn = $('.add-to-quote');
-        $btn.data('id', productId);
-        $btn.data('name', product.name);
-        $btn.data('image', product.images[0]);
+        $btn.attr('data-id', productId); // Use .attr() for data attributes to ensure DOM updates for CSS/selectors if needed, though .data() updates memory. 
+        // Best practice: update both if you rely on attribute selectors, but .data() is usually enough for jQuery. 
+        // However, the previous verification showed it didn't update. Let's force attribute update.
+        $btn.attr('data-id', productId);
+        $btn.attr('data-name', product.name);
+        $btn.attr('data-image', product.images[0]);
         // url is already correct (self)
 
         // Update document title
